e02c4f7e35d156a5c3fd3893f817135a8a07e28c
VertikaSahu <>
2022-04-06 14:24:14 +0000

    Initial commit

---
+++ filter_script.js
@@ -0,0 +1,16 @@
+function check() {
+
+  if ( document.getElementById("pr1").checked ==  true) {
+    document.getElementById("apply").href = "pr-1.html";
+
+  }
+  else if ( document.getElementById("pr2").checked ==  true) {
+    document.getElementById("apply").href = "pr-2.html";
+  }
+  else if ( document.getElementById("pr3").checked ==  true) {
+    document.getElementById("apply").href = "pr-3.html";
+  }
+  else if ( document.getElementById("pr4").checked ==  true) {
+    document.getElementById("apply").href = "pr-4.html";
+  }
+}
---
+++ script.js
@@ -0,0 +1,7 @@
+function unhide() {
+  var x = document.getElementById("custom").value;
+  if(x == 3) {
+    var y = document.getElementById("appear");
+    x.style.display = "block";
+  }
+}
